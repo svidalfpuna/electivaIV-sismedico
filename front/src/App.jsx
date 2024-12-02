f0ff8a11431a17a6f78afd89ee6435116a2f7c12
rdrivass <168840143+rdrivass@users.noreply.github.com>
2024-12-01 22:05:57 -0300

    routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Login from './components/Login.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import PacienteRegister from './components/PacienteRegister.jsx';
 import RegisterFicha from './components/fichaClinica.jsx';
 import Home from './pages/home.jsx';
 
 function App() {
   return <Home></Home>;
+
+  return (
+    <Router>
+      <Routes>
+        {/* Redirige a login si el usuario no está autenticado */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* Login */}
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/home" element={user ? <Home user={user} /> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
